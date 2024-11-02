00bad4123e2f7cbfad578f72a2165252cd4c42af
Veritas1814 <matviyshumylovych@gmail.com>
2024-11-02 19:51:11 +0200

    first commit

---
+++ src/main/java/ucu/edu/Lab82Application.java
@@ -0,0 +1,14 @@
+package ucu.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab82Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab82Application.class, args);
+	}
+
+
+}
---
+++ src/test/java/ucu/edu/Lab82ApplicationTests.java
@@ -0,0 +1,13 @@
+package ucu.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab82ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
